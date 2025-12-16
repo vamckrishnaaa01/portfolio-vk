94d648987cd6b918aae4e781eccbd2a8a439c42a
vamsi krishna G <imvkkrishna01@gmail.com>
2025-12-16 08:47:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
